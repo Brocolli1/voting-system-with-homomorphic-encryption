1fb19be3ca684b0fa1dd2f599c392efe41463636
Brocolli1 <mateuszkolakowski98@gmail.com>
2021-04-27 14:26:14 +0200

    first build

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+#include <helib/helib.h>
+
+//using namespace std;
+using namespace helib;
+
+int main(int argc, char* argv[])
+{
+    float a = sqrt(16) * M_PI;
+    printf("asjgvhcvh\n");
+    return 0;
+}
